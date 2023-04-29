9072d62004914bceaa30c4cdd5c01874364fb38b
Snezh88k <snezh88k@gmail.com>
2023-04-29 19:24:02 +0300

    feat: added Shift functional

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ import layout from "./layout.json" assert { type: "json" };
 const body = document.querySelector("body");
 const textarea = document.createElement("textarea");
 const keyboard = document.createElement("div");
+const title = document.createElement("h1");
 keyboard.classList.add("keyboard");
 textarea.classList.add("textarea");
+textarea.setAttribute("rows", "10");
+textarea.setAttribute("cols", "50");
+title.innerText = "Virtual Keyboard";
+
+body.appendChild(title);
 body.appendChild(textarea);
 body.appendChild(keyboard);
 
@@ -22,11 +28,35 @@ function interactiveClick(elem) {
   }, 400);
 }
 
+let shiftActive = false;
+
 // Создание кнопки
 
+const makeValueKey = () => {
+  for (let i = 0; i < keys.length; i++) {
+    if (language === "ru") {
+      if (shiftActive) {
+        layout[i].ru_shift
+          ? (keys[i].innerHTML = layout[i].ru_shift)
+          : (keys[i].innerHTML = layout[i].ru);
+      } else {
+        keys[i].innerHTML = layout[i].ru;
+      }
+    } else {
+      if (shiftActive) {
+        layout[i].en_shift
+          ? (keys[i].innerHTML = layout[i].en_shift)
+          : (keys[i].innerHTML = layout[i].ru);
+      } else {
+        keys[i].innerHTML = layout[i].en;
+      }
+    }
+  }
+};
+
 const makeKey = (value) => {
-  const { ru, en, type, keyCode, size } = value;
-  // console.log(ru, en, type, keyCode);
+  const { ru, en, ru_shift, en_shift, type, keyCode, size, eCode } = value;
+
   const key = document.createElement("div");
   keyboard.appendChild(key);
   key.classList.add("key");
@@ -42,38 +72,48 @@ const makeKey = (value) => {
   key.setAttribute("keyValueEn", en);
   key.setAttribute("keyType", type);
   key.setAttribute("keyCode", keyCode);
+  key.setAttribute("eCode", eCode);
 
-  if (language === "en") {
-    key.setAttribute("upperCaseName", en.toUpperCase());
-    key.innerHTML = en;
+  if (language === "ru") {
+    if (shiftActive) {
+      ru_shift ? (key.innerText = ru_shift) : (key.innerText = ru);
+    } else {
+      key.innerText = ru;
+    }
   } else {
-    key.setAttribute("upperCaseName", ru.toUpperCase());
-    key.innerHTML = ru;
+    if (shiftActive) {
+      en_shift ? (key.innerText = en_shift) : (key.innerText = ru);
+    } else {
+      key.innerText = en;
+    }
   }
 };
-for (let i = 0; i < layout.length; i++) {
-  makeKey(layout[i]);
-}
+
+const makeKeyboard = () => {
+  for (let i = 0; i < layout.length; i++) {
+    makeKey(layout[i]);
+  }
+};
+
+makeKeyboard();
+
+const keys = document.querySelectorAll(".key");
 
 //Добавление стилей на кнопки при нажатии
 
-const keys = document.querySelectorAll(".key");
 window.addEventListener("keydown", (e) => {
   textarea.focus();
+
   if (e.code === "Tab") {
     e.preventDefault();
     tabSpace();
   }
-  // console.log(e.key, "e.key");
-  // console.log(e.shiftKey, "e.shift");
-  // console.log(e.code, "e.code");
+
+  console.log(e.code, "e.code");
+  console.log(e.shiftKey);
+
   for (let i = 0; i < keys.length; i++) {
-    // console.log(keys[i].getAttribute("keyCode"), "keyCode");
-    if (
-      e.key === keys[i].getAttribute("keyname") ||
-      e.key === keys[i].getAttribute("upperCaseName") ||
-      e.code === keys[i].getAttribute("keyCode")
-    ) {
+    if (e.code === keys[i].getAttribute("ecode")) {
       keys[i].classList.add("active");
     }
   }
@@ -84,9 +124,8 @@ window.addEventListener("keydown", (e) => {
 window.addEventListener("keyup", (e) => {
   for (let i = 0; i < keys.length; i++) {
     if (
-      e.key === keys[i].getAttribute("keyname") ||
-      e.key === keys[i].getAttribute("upperCaseName") ||
-      e.code === keys[i].getAttribute("keyCode")
+      e.code === keys[i].getAttribute("keyCode") ||
+      e.code === keys[i].getAttribute("eCode")
     ) {
       keys[i].classList.remove("active");
     }
@@ -99,7 +138,16 @@ const standartKeys = document.querySelectorAll('[keyType="standart"]');
 for (let i = 0; i < standartKeys.length; i++) {
   standartKeys[i].addEventListener("mousedown", (e) => {
     textarea.focus();
-    textarea.value += standartKeys[i].getAttribute("keyValueRu");
+
+    let pointer = textarea.selectionStart;
+
+    textarea.value =
+      textarea.value.slice(0, pointer) +
+      standartKeys[i].getAttribute("keyValueRu") +
+      textarea.value.slice(pointer, textarea.value.length);
+
+    textarea.selectionStart = pointer + 1;
+    textarea.selectionEnd = pointer + 1;
 
     interactiveClick(standartKeys[i]);
   });
@@ -108,7 +156,7 @@ for (let i = 0; i < standartKeys.length; i++) {
 //Функционал кнопки "Backspace"
 
 const backspace = document.querySelector('[keyType="Backspace"]');
-backspace.addEventListener("mousedown", (e) => {
+backspace.addEventListener("mousedown", () => {
   interactiveClick(backspace);
 
   let pointer = textarea.selectionStart;
@@ -141,3 +189,51 @@ function tabSpace() {
   textarea.selectionStart = pointer + 4;
   textarea.selectionEnd = pointer + 4;
 }
+
+//Функционал кнопки "Enter"
+
+const ENTER = document.querySelector('[keyType="Enter"]');
+
+ENTER.addEventListener("mousedown", (e) => {
+  enterClick();
+});
+
+function enterClick() {
+  let pointer = textarea.selectionStart;
+
+  textarea.value =
+    textarea.value.slice(0, pointer) +
+    "\n" +
+    textarea.value.slice(pointer, textarea.value.length);
+  textarea.selectionStart = pointer + 1;
+  textarea.selectionEnd = pointer + 1;
+}
+
+const SHIFT = document.querySelector('[keyType="ShiftRight"]');
+console.log(SHIFT);
+SHIFT.addEventListener("keydown", (e) => {
+  console.log(e);
+});
+
+//Фунцционал кнопки "Shift"
+
+var handler = function (e) {
+  if (e.code === "ShiftLeft" || e.code === "ShiftRight") {
+    console.log("aaa", e);
+    shiftActive = true;
+    makeValueKey();
+
+    this.removeEventListener("keydown", handler);
+    this.addEventListener("keyup", (e) => {
+      if (e.code === "ShiftLeft" || e.code === "ShiftRight") {
+        console.log("dasdsa");
+        shiftActive = false;
+        makeValueKey();
+        window.addEventListener("keydown", handler);
+      }
+    });
+  }
+};
+window.addEventListener("keydown", handler);
+
+//Фунцционал кнопки "Alt" и "Ctrl"
