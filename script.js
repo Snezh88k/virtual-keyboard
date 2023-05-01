e5eb3a3d4543d67dbd6a95e7a9fc2146749de230
Snezh88k <snezh88k@gmail.com>
2023-05-01 07:42:26 +0300

    feat: added space key  funcinal

--- script.js
+++ script.js
@@ -17,8 +17,12 @@ window.addEventListener("click", (e) => {
   textarea.focus();
 });
 
-let language = "en";
+let language = localStorage.getItem("language")
+  ? localStorage.getItem("language")
+  : "en";
+
 let shiftActive = false;
+let capsLockActive = false;
 
 function interactiveClick(elem) {
   elem.classList.add("active");
@@ -27,12 +31,12 @@ function interactiveClick(elem) {
   }, 400);
 }
 
-//Переписывание значения кнопок
+//Работа капса
 
-const makeValueKey = () => {
+const makeCapsKey = () => {
   for (let i = 0; i < keys.length; i++) {
     if (language === "ru") {
-      if (shiftActive) {
+      if (capsLockActive && layout[i].capsRu) {
         if (layout[i].ru_shift) {
           keys[i].innerHTML = layout[i].ru_shift;
           keys[i].setAttribute("value", layout[i].ru_shift);
@@ -45,7 +49,7 @@ const makeValueKey = () => {
         keys[i].setAttribute("value", layout[i].ru);
       }
     } else {
-      if (shiftActive) {
+      if (capsLockActive && layout[i].capsEn) {
         if (layout[i].en_shift) {
           keys[i].innerHTML = layout[i].en_shift;
           keys[i].setAttribute("value", layout[i].en_shift);
@@ -56,6 +60,74 @@ const makeValueKey = () => {
       } else {
         keys[i].innerHTML = layout[i].en;
         keys[i].setAttribute("value", layout[i].en);
+      }
+    }
+  }
+};
+
+//Работа шифта
+
+const makeValueKey = () => {
+  for (let i = 0; i < keys.length; i++) {
+    if (language === "ru") {
+      if (shiftActive) {
+        if (capsLockActive && layout[i].capsRu) {
+          keys[i].innerHTML = layout[i].ru;
+          keys[i].setAttribute("value", layout[i].ru);
+        }
+
+        if (capsLockActive && !layout[i].capsRu && layout[i].ru_shift) {
+          keys[i].innerHTML = layout[i].ru_shift;
+          keys[i].setAttribute("value", layout[i].ru_shift);
+        }
+
+        if (!capsLockActive && layout[i].ru_shift) {
+          keys[i].innerHTML = layout[i].ru_shift;
+          keys[i].setAttribute("value", layout[i].ru_shift);
+        }
+      } else {
+        if (capsLockActive && layout[i].capsRu) {
+          keys[i].innerHTML = layout[i].ru_shift;
+          keys[i].setAttribute("value", layout[i].ru_shift);
+        }
+        if (capsLockActive && !layout[i].capsRu) {
+          keys[i].innerHTML = layout[i].ru;
+          keys[i].setAttribute("value", layout[i].ru);
+        }
+        if (!capsLockActive) {
+          keys[i].innerHTML = layout[i].ru;
+          keys[i].setAttribute("value", layout[i].ru);
+        }
+      }
+    } else {
+      if (shiftActive) {
+        if (capsLockActive && layout[i].capsEn) {
+          keys[i].innerHTML = layout[i].en;
+          keys[i].setAttribute("value", layout[i].en);
+        }
+
+        if (capsLockActive && !layout[i].capsEn && layout[i].en_shift) {
+          keys[i].innerHTML = layout[i].en_shift;
+          keys[i].setAttribute("value", layout[i].en_shift);
+        }
+
+        if (!capsLockActive && layout[i].en_shift) {
+          keys[i].innerHTML = layout[i].en_shift;
+          keys[i].setAttribute("value", layout[i].en_shift);
+        }
+      } else {
+        if (capsLockActive && layout[i].capsEn) {
+          keys[i].innerHTML = layout[i].en_shift;
+          keys[i].setAttribute("value", layout[i].en_shift);
+        }
+        if (capsLockActive && !layout[i].capsEn) {
+          keys[i].innerHTML = layout[i].en;
+          keys[i].setAttribute("value", layout[i].en);
+        }
+        if (!capsLockActive) {
+          keys[i].innerHTML = layout[i].en;
+          keys[i].setAttribute("value", layout[i].en);
+        }
       }
     }
   }
@@ -122,44 +194,6 @@ const standartKeys = document.querySelectorAll('[keyType="standart"]');
 
 for (let i = 0; i < standartKeys.length; i++) {
   standartKeys[i].addEventListener("mousedown", (e) => {
-    textarea.focus();
-
-    let pointer = textarea.selectionStart;
-
-    textarea.value =
-      textarea.value.slice(0, pointer) +
-      standartKeys[i].getAttribute("value") +
-      textarea.value.slice(pointer, textarea.value.length);
-
-    textarea.selectionStart = pointer + 1;
-    textarea.selectionEnd = pointer + 1;
-
-    interactiveClick(standartKeys[i]);
-  });
-}
-
-window.addEventListener("keydown", (e) => {
-  let pointer = textarea.selectionStart;
-
-  for (let i = 0; i < keys.length; i++) {
-    if (
-      e.code === keys[i].getAttribute("eCode") &&
-      keys[i].getAttribute("keyType") === "standart"
-    ) {
-      e.preventDefault();
-      textarea.value =
-        textarea.value.slice(0, pointer) +
-        keys[i].getAttribute("value") +
-        textarea.value.slice(pointer, textarea.value.length);
-    }
-  }
-
-  textarea.selectionStart = pointer + 1;
-  textarea.selectionEnd = pointer + 1;
-});
-
-for (let i = 0; i < standartKeys.length; i++) {
-  standartKeys[i].addEventListener("keydown", (e) => {
     e.preventDefault();
     textarea.focus();
 
@@ -177,6 +211,25 @@ for (let i = 0; i < standartKeys.length; i++) {
   });
 }
 
+window.addEventListener("keydown", (e) => {
+  for (let i = 0; i < keys.length; i++) {
+    if (
+      e.code === keys[i].getAttribute("eCode") &&
+      keys[i].getAttribute("keyType") === "standart"
+    ) {
+      e.preventDefault();
+      let pointer = textarea.selectionStart;
+      textarea.value =
+        textarea.value.slice(0, pointer) +
+        keys[i].getAttribute("value") +
+        textarea.value.slice(pointer, textarea.value.length);
+
+      textarea.selectionStart = pointer + 1;
+      textarea.selectionEnd = pointer + 1;
+    }
+  }
+});
+
 //Функционал кнопки "Backspace"
 
 const backspace = document.querySelector('[keyType="Backspace"]');
@@ -184,14 +237,23 @@ backspace.addEventListener("mousedown", () => {
   interactiveClick(backspace);
 
   let pointer = textarea.selectionStart;
-  if (pointer === 0) {
-    return;
+
+  if (textarea.selectionStart === textarea.selectionEnd) {
+    if (pointer === 0) {
+      return;
+    }
+    textarea.value =
+      textarea.value.slice(0, pointer - 1) +
+      textarea.value.slice(pointer, textarea.value.length);
+    textarea.selectionStart = pointer - 1;
+    textarea.selectionEnd = pointer - 1;
+  } else {
+    textarea.value =
+      textarea.value.slice(0, textarea.selectionStart) +
+      textarea.value.slice(textarea.selectionEnd, textarea.value.length);
+    textarea.selectionStart = pointer;
+    textarea.selectionEnd = pointer;
   }
-  textarea.value =
-    textarea.value.slice(0, pointer - 1) +
-    textarea.value.slice(pointer, textarea.value.length);
-  textarea.selectionStart = pointer - 1;
-  textarea.selectionEnd = pointer - 1;
 });
 
 //Функционал кнопки "Tab"
@@ -246,6 +308,7 @@ function enterClick() {
 
 const handler = function (e) {
   if (e.code === "ShiftLeft" || e.code === "ShiftRight") {
+    e.preventDefault();
     shiftActive = true;
     makeValueKey();
 
@@ -295,7 +358,12 @@ window.addEventListener("keydown", (e) => {
     if (e.code === "AltLeft") {
       altPress = true;
       if (ctrlPress) {
-        language = language === "en" ? "ru" : "en";
+        if (localStorage.getItem("language") === "en") {
+          localStorage.setItem("language", "ru");
+        } else {
+          localStorage.setItem("language", "en");
+        }
+        language = localStorage.getItem("language");
         makeValueKey();
       }
     }
@@ -304,11 +372,14 @@ window.addEventListener("keydown", (e) => {
 
 window.addEventListener("keydown", (e) => {
   if (e.code === "ControlLeft") {
-    console.log("тык");
     ctrlPress = true;
     if (altPress) {
-      console.log("Переключаем язык");
-      language = language === "en" ? "ru" : "en";
+      if (localStorage.getItem("language") === "en") {
+        localStorage.setItem("language", "ru");
+      } else {
+        localStorage.setItem("language", "en");
+      }
+      language = localStorage.getItem("language");
       makeValueKey();
     }
   }
@@ -321,4 +392,69 @@ window.addEventListener("keyup", (e) => {
   if (e.code === "ControlLeft") {
     ctrlPress = false;
   }
+});
+
+//Фунционал кнопки "CAPSLOCK"
+
+const CAPS_LOCK = document.querySelector('[ecode="CapsLock"]');
+
+CAPS_LOCK.addEventListener("mousedown", (e) => {
+  capsLockActive = !capsLockActive;
+  CAPS_LOCK.classList.toggle("caps-active");
+  makeCapsKey();
+});
+
+const capsActivate = function (e) {
+  if (e.code === "CapsLock") {
+    capsLockActive = !capsLockActive;
+    CAPS_LOCK.classList.toggle("caps-active");
+    makeCapsKey();
+    this.removeEventListener("keydown", capsActivate);
+    this.addEventListener("keyup", (e) => {
+      if (e.code === "CapsLock") {
+        window.addEventListener("keydown", capsActivate);
+      }
+    });
+  }
+};
+window.addEventListener("keydown", capsActivate);
+
+//Фунционал кнопки "Space"
+
+const SPACE = document.querySelector('[ecode="Space"]');
+
+SPACE.addEventListener("mousedown", (e) => {
+  interactiveClick(SPACE);
+  Space();
+});
+
+function Space() {
+  let pointer = textarea.selectionStart;
+
+  textarea.value =
+    textarea.value.slice(0, pointer) +
+    " " +
+    textarea.value.slice(pointer, textarea.value.length);
+  textarea.selectionStart = pointer + 1;
+  textarea.selectionEnd = pointer + 1;
+}
+
+// Функционал кнопки "Delete"
+
+const DELETE = document.querySelector('[ecode="Delete"]');
+DELETE.addEventListener("mousedown", () => {
+  interactiveClick(DELETE);
+
+  let pointer = textarea.selectionStart;
+
+  if (textarea.selectionStart === textarea.selectionEnd) {
+    textarea.value =
+      textarea.value.slice(0, pointer) +
+      textarea.value.slice(pointer + 1, textarea.value.length);
+  }
+  textarea.value =
+    textarea.value.slice(0, textarea.selectionStart) +
+    textarea.value.slice(textarea.selectionEnd, textarea.value.length);
+  textarea.selectionStart = pointer;
+  textarea.selectionEnd = pointer;
 });
